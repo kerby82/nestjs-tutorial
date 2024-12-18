7bcbb9c484d835eaf7f38c73c6b49672cbe509ac
Andrea Cirulli <andrea.cirulli@instructure.com>
2024-12-18 12:26:19 -0600

    3.19 connecting to the database
    I am using postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'example',
+      database: 'nest-events',
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
