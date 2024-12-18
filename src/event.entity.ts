8b6bf9f239ce1a1a69a3a1c900f4660a3a70857a
Andrea Cirulli <andrea.cirulli@instructure.com>
2024-12-18 12:37:52 -0600

    3.21 the entity primary keys and columns

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('events')
 export class Event {
+  @PrimaryGeneratedColumn() // auto-incrementing primary key
   id: number;
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @Column()
   when: Date;
+  @Column()
   address: string;
 }
