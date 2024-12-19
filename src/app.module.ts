d7dab2438f6c9d9ceb6f01efb0d7cebc0f7f1130
Andrea Cirulli <andrea.cirulli@instructure.com>
2024-12-19 12:34:13 -0600

    3.36 custom configuration files and options

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsModule } from './events/events.module';
-import { Event } from './events/event.entity';
 import { AppJapanService } from './app.japan.service';
 import { AppDummy } from './app.dummy';
 import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // this is used to load the .env file
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Event],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({
+      isGlobal: true, // global to all modules
+      envFilePath: '.env',
+      load: [ormConfig],
+      expandVariables: true,
+    }), // this is used to load the .env file
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV === 'production' ? ormConfigProd : ormConfig,
+    }), // this is used to connect to the database
     EventsModule, // this does the same as the entities array in the TypeOrmModule.forRoot() call
   ],
   controllers: [AppController],
