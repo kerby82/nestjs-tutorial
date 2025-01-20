fcd20104482ff1333b2ed2f4e9d1136233ac298f
Andrea Cirulli <andrea.cirulli@instructure.com>
2025-01-20 09:46:04 -0500

    nest test utilities

---
+++ src/events/events.service.spec.ts
@@ -0,0 +1,35 @@
+import { EventsService } from './events.service';
+import { Event } from './event.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Test } from '@nestjs/testing';
+
+describe('EventsService', () => {
+  let service: EventsService;
+  let repository: Repository<Event>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        EventsService,
+        {
+          provide: getRepositoryToken(Event),
+          useValue: {
+            save: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            delete: jest.fn(),
+            where: jest.fn(),
+            execute: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<EventsService>(EventsService);
+    repository = module.get<Repository<Event>>(getRepositoryToken(Event));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
