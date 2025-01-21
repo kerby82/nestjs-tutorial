882ca693a3ec64634fa93f698c8b3f667152e386
Andrea Cirulli <andrea.cirulli@instructure.com>
2025-01-21 10:27:59 -0500

    graphql query arguments

--- src/school/teacher.resolver.ts
+++ src/school/teacher.resolver.ts
@@ -1,6 +1,6 @@
 import { Resolver } from '@nestjs/graphql';
 import { Teacher } from './teacher.entity';
-import { Query } from '@nestjs/graphql';
+import { Query, Args } from '@nestjs/graphql';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -14,6 +14,17 @@ export class TeacherResolver {
   @Query(() => [Teacher])
   public async teachers(): Promise<Teacher[]> {
     return await this.teacherRepository.find({
+      relations: ['subjects'],
+    });
+  }
+
+  @Query(() => Teacher)
+  public async teacher(
+    @Args('id', { type: () => Number })
+    id: number,
+  ): Promise<Teacher | undefined> {
+    return await this.teacherRepository.findOneOrFail({
+      where: { id },
       relations: ['subjects'],
     });
   }
